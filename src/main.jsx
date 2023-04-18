02ffd5680d4ceb49a3847d61728b4da977a74520
Sireethon Bunchongpru <werrty65@gmail.com>
2023-04-19 00:07:54 +0700

    Change for the better

--- src/main.jsx
+++ src/main.jsx
@@ -25,5 +25,8 @@ const router = HashRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  
 )
