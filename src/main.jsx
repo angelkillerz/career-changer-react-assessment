6518ecd1953295bd35c0973665cd90be40864432
Sireethon Bunchongpru <werrty65@gmail.com>
2023-04-19 00:05:51 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {
   HashRouter
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = HashRouter([
   {
     path: '/career-changer-react-assessment',
     element: <Home />
